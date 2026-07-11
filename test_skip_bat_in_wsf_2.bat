83ee9959c5508a4ce467a2502b1b81b80dfe8502
andry81 <andry@inbox.ru>
2026-07-11 20:06:38 +0300

    bat in wsf skip

---
+++ test_skip_bat_in_wsf_2.bat
@@ -0,0 +1,18 @@
+@<!-- rem bat in vbs skip
+
+@echo off
+
+setlocal
+
+"%SystemRoot%\System32\cscript.exe" //NOLOGO //JOB:TEST_SKIP_BAT_IN_WSF "%~f0?.wsf"
+exit /b 0
+
+rem end of bat -->
+
+<package>
+  <job id="TEST_SKIP_BAT_IN_WSF">
+    <script language="VBScript">
+      WScript.Echo "PASSED."
+    </script>
+  </job>
+</package>
